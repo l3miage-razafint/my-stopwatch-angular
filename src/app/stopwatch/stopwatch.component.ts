e67ee153b93019cc0d3055a37ac1269147e21042
l3miage-razafint <Timoty.Razafindrabe@etu.univ-grenoble-alpes.fr>
2023-04-03 18:48:02 +0200

    Setting up the project

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss']
+})
+export class StopwatchComponent {
+
+}
